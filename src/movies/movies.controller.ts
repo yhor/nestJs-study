67b498e99b8bda6324c53efb0cd1f7e383001845
ysy <ysy@kipis.co.kr>
2021-09-14 20:28:00 +0900

    create controller, service

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly movieService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.movieService.getAll();
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') movieId: string) {
+    return this.movieService.getOne(movieId);
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    return this.movieService.create(movieData);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return this.movieService.deleteOne(movieId);
+  }
+
+  @Patch('/:id')
+  path(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updateMovie: movieId,
+      ...updateData,
+    };
+  }
+}
